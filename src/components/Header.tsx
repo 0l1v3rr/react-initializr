decb5427853e95439defaf23130825c28bd6a380
Olivér <oliver.mrakovics@gmail.com>
2022-08-13 10:42:49 +0200

    added the ability to donwload the zip and the package.json

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -17,6 +17,9 @@ import {
 import Button from "./Button";
 import ButtonLink from "./ButtonLink";
 import { useState } from 'react';
+import JSZip from 'jszip';
+import { saveAs } from 'file-saver';
+import axios from 'axios';
 
 const Header = () => {
   const [copyText, setCopyText] = useState("Copy Link");
@@ -56,6 +59,87 @@ const Header = () => {
     setTimeout(() => setCopyText("Copy Link"), 1000);
   };
 
+  const generateZip = async () => {
+    // creating the ZIP and a folder to append the files
+    const zip = new JSZip();
+    const project = zip.folder(name);
+
+    // creating the package.json file
+    const packageJson = await generatePackageJson()
+    project?.file("package.json", packageJson);
+
+    // donwloading the ZIP
+    zip.generateAsync({ type: "blob" })
+      .then(res => saveAs(res, `${name === "" ? "project" : name}.zip`));
+  };
+
+  const generatePackageJson = async () => {
+    let dependencies: any = {};
+
+    // adding some libraries manually
+    // firstly, finding out the latest version each of them
+    let testingJestVersion = await getLatestDependencyVersion("@testing-library/jest-dom");
+    let testingReactVersion = await getLatestDependencyVersion("@testing-library/react");
+    let testingUserEventVersion = await getLatestDependencyVersion("@testing-library/user-event");
+
+    // then append
+    dependencies["@testing-library/jest-dom"] = `^${testingJestVersion}`;
+    dependencies["@testing-library/react"] = `^${testingReactVersion}`;
+    dependencies["@testing-library/user-event"] = `^${testingUserEventVersion}`;
+
+    // iterating through the packages and adding each of them 
+    // to the dependencies
+    for(let i of packages) {
+      dependencies[i.packageName] = `^${i.version}`;
+    }
+    
+    // package.json object
+    const result: any = {
+      name: name === "" ? "my-project" : version,
+      version: version === "" ? "1.0.0" : version,
+      private: true,
+      dependencies: dependencies,
+      scripts: {
+        start: "react-scripts start",
+        build: "react-scripts build",
+        test: "react-scripts test",
+        eject: "react-scripts eject"
+      },
+      eslintConfig: {
+        extends: ["react-app", "react-app/jest"]
+      },
+    };
+
+    // only append if the current value is not empty
+    if(description !== "") result.description = description;
+    if(author !== "") result.author = author;
+    if(license !== "") result.license = license;
+
+    if(gitRepo !== "") {
+      result.repository = {
+        type: "git",
+        url: gitRepo
+      };
+      result.bugs = {
+        url: `${gitRepo}/issues`
+      };
+    }
+
+    // converting the object to a JSON
+    return JSON.stringify(result);
+  }
+
+  const getLatestDependencyVersion = async (name: string) => {
+    let version = "";
+    
+    await axios.get(`https://registry.npmjs.org/${name}/latest`)
+      .then(res => {
+        version = res.data.version;
+      });
+
+    return version;
+  }
+
   return (
     <div className="flex items-center justify-center md:justify-between w-full h-fit border-b-2 border-solid 
       border-zinc-800 sm:px-12 px-4 py-4 sticky top-0 bg-zinc-900 shadow-md z-10">
@@ -76,7 +160,7 @@ const Header = () => {
         <Button
           text="Generate ZIP"
           icon={BsFileZip}
-          onClick={() => {}}
+          onClick={generateZip}
         />
 
         <Button
