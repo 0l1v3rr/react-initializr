5d224ba7cf1904ac198fc94866384c6734667b40
Olivér <oliver.mrakovics@gmail.com>
2022-08-15 10:13:55 +0200

    package.json bug fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -153,6 +153,18 @@ const Header = () => {
       eslintConfig: {
         extends: ["react-app", "react-app/jest"]
       },
+      browserslist: {
+        production: [
+          ">0.2%",
+          "not dead",
+          "not op_mini all"
+        ],
+        development: [
+          "last 1 chrome version",
+          "last 1 firefox version",
+          "last 1 safari version"
+        ]
+      }
     };
 
     // only append if the current value is not empty
