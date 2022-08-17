dbc6a27c77f7b45c04e95c2d2b5a2030f230a3ec
Olivér <oliver.mrakovics@gmail.com>
2022-08-17 10:18:21 +0200

    the dependency counter now doesn't count the default 6 dependencies

--- src/components/ButtonLink.tsx
+++ src/components/ButtonLink.tsx
@@ -12,7 +12,7 @@ const ButtonLink:FC<ButtonLinkProps> = (props) => {
     <a href={props.to} rel="noreferrer" target="_blank" className="font-semibold px-4 
       py-2 border-2 border-solid border-zinc-800 rounded-md flex items-center justify-center 
       gap-2 leading-7 hover:border-zinc-700 color-react-hover hover:bg-white/[0.04] 
-      transition-all hover:scale-[103%] hover:shadow-md">
+      transition-all hover:scale-[103%] hover:shadow-md text-[1em]">
       <props.icon />
 
       <span className="text-zinc-200">{props.text.toUpperCase()}</span>
