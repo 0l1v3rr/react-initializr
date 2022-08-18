8aee433c596b715745d2dd4588512eb02abb2bf2
karancoder <jivanikaran@gmail.com>
2022-08-19 00:13:54 +0530

    bug fix

--- src/components/BlurOverlay.tsx
+++ src/components/BlurOverlay.tsx
@@ -1,8 +1,19 @@
-const BlurOverlay = (props: { isActive: boolean }) => {
-  return (
-    <div className={`w-screen h-screen fixed top-0 left-0 transition-all duration-300 
-      z-20 ${props.isActive ? "backdrop-blur-sm" : "pointer-events-none"}`} />
-  );
-}
+import { useEffect } from "react";
 
-export default BlurOverlay
+const BlurOverlay = (props: { isActive: boolean }) => {
+    useEffect(() => {
+        if (props.isActive) {
+            document.body.style.overflow = "hidden";
+        } else {
+            document.body.style.overflow = "unset";
+        }
+    }, [props.isActive]);
+    return (
+        <div
+            className={`w-screen h-screen fixed top-0 left-0 transition-all duration-300 
+      z-20 ${props.isActive ? "backdrop-blur-sm" : "pointer-events-none"}`}
+        />
+    );
+};
+
+export default BlurOverlay;
