1b009e3edcf3994726927f65f23b323e4b11d09b
Olivér <oliver.mrakovics@gmail.com>
2022-09-03 15:55:33 +0200

    validated the homepage and the gitrepo fields with regex

--- src/validate.ts
+++ src/validate.ts
@@ -15,12 +15,44 @@ export const validateProject = (
   }
 
   // kebab-case regex
-  if (!name.match(/^([a-z][a-z0-9]*)(-[a-z0-9]+)*$/g)) {
+  if (!name.match(/^([a-z][a-z0-9]*)(-[a-z0-9]+)*$/gi)) {
     return {
       isValid: false,
       errorMessage:
-        "The name should be kebab-case and longer than 3 characters.",
+        "The Name should be kebab-case and longer than 3 characters.",
     };
+  }
+
+  // if the homepage is not valid
+  if (homePage.trim() !== "") {
+    if (
+      !homePage
+        .trim()
+        .match(
+          /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/gi
+        )
+    ) {
+      return {
+        isValid: false,
+        errorMessage: "The Homepage should be a valid URL.",
+      };
+    }
+  }
+
+  if (gitRepo.trim() !== "") {
+    if (
+      !gitRepo
+        .trim()
+        .match(
+          /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)(\.git)/gi
+        )
+    ) {
+      return {
+        isValid: false,
+        errorMessage:
+          "Git Repository should be a valid URL that ends with '.git'.",
+      };
+    }
   }
 
   // if everything is valid
